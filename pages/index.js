5584f86d57cec229ed1991409e1d6b6522aa78cb
yufan <yijianz7788@gmail.com>
2023-08-28 14:33:22 +0800

    🪘fix: path

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import Homepage from './Homepage/index.tsx'
+import Homepage from './homePage'
 
 export default function Home() {
   return (
